8bea21448bb5e8ee9c55548e1a291e0462f075c4
preska <wolfwar@outlook.com>
2023-01-02 11:24:06 +0100

    not found

---
+++ app/todos/[todoId]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <div>Woops</div>;
+};
+
+export default NotFound;
--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,23 +1,57 @@
+import { NOTFOUND } from 'dns';
 import React from 'react';
+import { Todo } from '../../../typings';
+import { notFound } from 'next/navigation';
+
 type PageProps = {
   params: {
     todoId: string;
   };
 };
 
+export const dynamicParams = true; // try SSR if no SSG
+
 const fetchTodo = async (todoId: string) => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    { cache: 'no-cache' } // - SSR
+    // { cache: 'force-cache' } - static site generation
+    // { next: {revalidate: 60} } - svakih 60 sec revalidate page, ISR
   );
 
-  const todo = await res.json();
+  const todo: Todo = await res.json();
 
   return todo;
 };
 
 const TodoPage = async ({ params: { todoId } }: PageProps) => {
   const todo = await fetchTodo(todoId);
-  return <div>{todo.title}</div>;
+
+  if (!todo.id) return notFound();
+
+  return (
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? 'yes' : 'no'}</p>
+      <p className="border-t border-black mt-5 text-right">
+        By User: {todo.userId}
+      </p>
+    </div>
+  );
 };
 
 export default TodoPage;
+
+export async function generateStaticParams() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
+  const todos: Todo[] = await res.json();
+
+  const trimmedTodos = todos.splice(0, 10);
+
+  // static site generation
+  return trimmedTodos.map((todo) => ({
+    todoId: todo.id.toString(),
+  }));
+}
